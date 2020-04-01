7dfbefd798fe226ff6a68c8030a7b73d8234bc2f
Jordan Chapuy <chapuyj@gmail.com>
2020-04-01 14:28:02 +0200

    Mob-programming #2 - short exploration for sourcery in order to list automatically all test classes and methods

---
+++ Sources/beUnitRunner/Sourcery/Generated/FindAllTestCases.generated.swift
@@ -0,0 +1,13 @@
+// Generated using Sourcery 0.17.0 — https://github.com/krzysztofzablocki/Sourcery
+// DO NOT EDIT
+
+import Foundation
+import beUnit
+
+class SourceryTestCasesProvider: TestCasesProvider {
+    func findAll() -> [TestCase] {
+        return [
+            //TODO: template here
+        ]
+    }
+}
